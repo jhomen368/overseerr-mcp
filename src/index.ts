afe8fc8ceaf35faf2d7df63d3efdae84a3c0bd43
jhomen368 <joshua.homen@gmail.com>
2026-04-04 00:20:46 -0700

    fix(index): remove unnecessary fallback for media title
    Signed-off-by: jhomen368 <joshua.homen@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1702,7 +1702,7 @@ class OverseerrServer {
 
     // Get media title with caching
     const detailsCacheKey = { mediaType, mediaId };
-    let mediaTitle = 'Unknown Media';
+    let mediaTitle;
 
     let details = this.cache.get<MediaDetails>('mediaDetails', detailsCacheKey);
     if (!details) {
